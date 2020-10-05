1b59d75f7cbe8c0b42b97e477a499a3c2c856779
MagicPojska <pojskicsafet@gmail.com>
2020-10-05 13:41:16 +0200

    Made pipes

--- flappy_bird_main.py
+++ flappy_bird_main.py
@@ -1,11 +1,27 @@
-import pygame, sys
-
-
+import pygame, sys, random
 
 def move_floor():                               #function for moving the base
     screen.blit(floor_surface, (floor_x_pos, 900))
     screen.blit(floor_surface, (floor_x_pos + 576, 900))    #(we added another image to the right so it can loop)
 
+def create_pipe():
+    random_pipe_pos = random.choice(pipe_height)
+    bottom_pipe = pipe_surface.get_rect(midtop = (700, random_pipe_pos))                #Creating bottom pipe with midtop
+    top_pipe = pipe_surface.get_rect(midbottom = (700, random_pipe_pos - 300))          #Creating top pipe with midbottop and moving it up a bit so we can have space between two pipes
+    return bottom_pipe, top_pipe
+
+def move_pipes(pipes):      #This function takes all the pipe rectangles and moves them to the left 
+    for pipe in pipes:
+        pipe.centerx -= 5
+    return pipes            #We return a new list of pipes
+
+def draw_pipes(pipes):
+    for pipe in pipes:
+        if pipe.bottom >= 1024:                 #We do this for the bottom pipes if they extend bellow 1024 pixels
+            screen.blit(pipe_surface,pipe)
+        else:                                   #And here we flip the pipe if its top pipe so we it can be placed nicely
+            flip_pipe = pygame.transform.flip(pipe_surface, False, True)    
+            screen.blit(flip_pipe, pipe)
 
 pygame.init()
 screen = pygame.display.set_mode((576,1024))    #setting the window size
@@ -26,7 +42,12 @@ bird_surface = pygame.image.load('assets/bluebird-midflap.png').convert()
 bird_surface = pygame.transform.scale2x(bird_surface)
 bird_rect = bird_surface.get_rect(center = (100,512))             #We are making a rectange around the image so we can later use it for colision(center of rectangle is at 100,512)
 
-
+pipe_surface = pygame.image.load('assets/pipe-green.png').convert()     #Adding pipes on the surface but without rectangles because we need a rectangle for each new pipe on the screen
+pipe_surface = pygame.transform.scale2x(pipe_surface)  
+pipe_list = []
+SPAWNPIPE = pygame.USEREVENT                                            #Adding an event that happens every 1200miliseconds (that means the pipe will apper on the screen from right every 1.2sec)
+pygame.time.set_timer(SPAWNPIPE, 1200)
+pipe_height = [400, 600, 800]
 
 #Making a screen to play on and our game loop:
 while True:     #we need a loopt so the game wont close after one run
@@ -38,19 +59,26 @@ while True:     #we need a loopt so the game wont close after one run
             if event.key == pygame.K_SPACE:
                 bird_movement = 0
                 bird_movement -= 12
+        if event.type == SPAWNPIPE:     #This event will create a new pipe and store it in the pipe_list
+            pipe_list.extend(create_pipe())
 
     #this all repeats every frame so if we increment a position of an asset for 1 it will move every time loops plays again (floor_x_pos += 1 as an example and the base will move to the right)
     screen.blit(bg_surface, (0,0))  #positioning assets on the screen
 
+    #Bird
     bird_movement += gravity
     bird_rect.centery += bird_movement
     screen.blit(bird_surface, bird_rect)
 
+    #Pipes
+    pipe_list = move_pipes(pipe_list)
+    draw_pipes(pipe_list)
+
+    #Floor
     floor_x_pos -= 1
     move_floor()
     if floor_x_pos <= -576:         #if the images go too far to the left we reset the position so we give the illusion that it is infinite
         floor_x_pos = 0
-
 
     pygame.display.update()
     clock.tick(120)
